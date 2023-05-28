a9046bd2db3ed1d7704cb2961e345b626bfa6da3
anibalr7 <anibalrarroyoaa@gmail.com>
2023-05-28 14:24:56 -0500

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,15 @@
+var colorButton = document.getElementById("colorButton");
+var mainElement = document.getElementById("myMain");
+
+colorButton.addEventListener("click", function() {
+  mainElement.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
